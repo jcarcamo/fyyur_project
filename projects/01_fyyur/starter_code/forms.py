a2159d48cf14c47999a05ef197c676f50493a71b
Juan Gonzalo Carcamo <jcarcamo@gmail.com>
2021-04-13 17:39:54 -0500

    Changes name of Artist field to seeking_venue, Showing artist by id

--- projects/01_fyyur/starter_code/forms.py
+++ projects/01_fyyur/starter_code/forms.py
@@ -231,8 +231,8 @@ class ArtistForm(Form):
     website = StringField(
         'website', validators=[URL()]
     )
-    seeking_talent = BooleanField(
-        'seeking_talent'
+    seeking_venue = BooleanField(
+        'seeking_venue'
     )
     seeking_description = StringField(
         'seeking_description'
